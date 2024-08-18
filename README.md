bdb469418fda4b992172dd6a1c37a83f3ebc8929
roylau <roy-lau@foxmail.com>
2024-08-18 22:07:20 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # learning_docker
-学习 docker
+
+> 学习 docker
+
+## 命令行
+```bash
+# 启动
+docker compose up
+# 后台启动
+docker compose up -d
+```
+
+
+## docker 文档
+
+- [下载](https://docs.docker.com/get-docker/)
+
+- dockerfile 配置 https://docs.docker.com/reference/dockerfile/
+
+- docker compose file 配置 https://docs.docker.com/compose/compose-file/
+
+- docker compose 导入其他 docker 配置 https://docs.docker.com/compose/multiple-compose-files/include/
+
+- vuejs demo https://github.com/docker/awesome-compose/blob/master/vuejs/compose.yaml
---
+++ openresty/lua_script/test.lua
@@ -0,0 +1,57 @@
+
+local str = "abcde"
+print("case 1:", str:sub(1, 2))
+print("case 2:", str.sub(str, 1, 2))
+
+ngx.log(ngx.INFO, " string:", str)
+
+-- ngx.STDERR     -- 标准输出
+-- ngx.EMERG      -- 紧急报错
+-- ngx.ALERT      -- 报警
+-- ngx.CRIT       -- 严重，系统故障，触发运维告警系统
+-- ngx.ERR        -- 错误，业务不可恢复性错误
+-- ngx.WARN       -- 告警，业务中可忽略错误
+-- ngx.NOTICE     -- 提醒，业务比较重要信息
+-- ngx.INFO       -- 信息，业务琐碎日志信息，包含不同情况判断等
+-- ngx.DEBUG      -- 调试
+
+ngx.say("<h1> test lua script</h1>")
+-- print(string.format("%s %02d/%02d/%d", "today is:", d, m, y))
+
+ngx.say("<h2> lua 基本数据类型</h2>")
+
+ngx.say('<li> type === ' .. type("hello world") .. '</li>')
+ngx.say('<li> type === ' .. type(print) .. '</li>')
+ngx.say('<li> type === ' .. type(true) .. '</li>')
+ngx.say('<li> type === ' .. type(360.0) .. '</li>')
+ngx.say('<li> type === ' .. type(nil) .. '</li>')
+
+
+
+ngx.say("<h2> lua 时间 脚本</h2>")
+
+ngx.say('<li>ngx.today() ---> ' .. ngx.today() .. '</li>')
+ngx.say('<li>ngx.time() ---> ' .. ngx.time() .. '</li>')
+ngx.say('<li>ngx.utctime() ---> ' .. ngx.utctime() .. '</li>')
+ngx.say('<li>ngx.localtime() ---> ' .. ngx.localtime() .. '</li>')
+ngx.say('<li>ngx.now() ---> ' .. ngx.now() .. '</li>')
+
+ngx.say("<h2> lua req 脚本</h2>")
+ngx.say('<li>ngx.req.get_method() ---> ' .. ngx.req.get_method() .. '</li>')
+-- ngx.say('<li>ngx.req.get_uri_args() ---> ' .. ngx.req.get_uri_args() .. '</li>')
+
+local function showReq()
+  local arg = ngx.req.get_uri_args()
+  for k,v in pairs(arg) do
+      ngx.say("[GET ] key:", k, " v:", v)
+  end
+
+  ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
+  local arg = ngx.req.get_post_args()
+  for k,v in pairs(arg) do
+      ngx.say("[POST] key:", k, " v:", v)
+  end
+
+end
+
+showReq()
